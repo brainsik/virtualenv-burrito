96ac92827a0eb3c8564a8d9620e72ffcbc87d146
jeremy avnet <brainsik-github@theory.org>
2012-05-18 15:52:24 -0700

    New upstream versions of all packages

--- test_download_md5.py
+++ test_download_md5.py
@@ -9,9 +9,9 @@ PYPI_MD5_URL = 'http://pypi.python.org/pypi?:action=show_md5&digest='
 
 PYPI_DOWNLOADS = {
     # filename: md5sum
-    'distribute-0.6.24.tar.gz': '17722b22141aba8235787f79800cc452',
-    'virtualenv-1.7.tar.gz': 'dcc105e5a3907a9dcaa978f813a4f526',
-    'virtualenvwrapper-2.11.1.tar.gz': 'dc65934feece5fd51fd0454114329392',
+    'distribute-0.6.27.tar.gz': 'ecd75ea629fee6d59d26f88c39b2d291',
+    'virtualenv-1.7.1.2.tar.gz': '3be8a014c27340f48b56465f9109d9fa',
+    'virtualenvwrapper-3.3.tar.gz': '7e334cec98d800dcd8e4959502616d16',
 }
 
 
