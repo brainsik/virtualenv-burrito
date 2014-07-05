0af94c832e596394b676c0d78e4d5c4ae08a47f1
jeremy avnet <brainsik-github@theory.org>
2014-07-05 13:57:46 -0700

    new upstream virtualenv

--- test_download.py
+++ test_download.py
@@ -12,7 +12,7 @@ PYPI_DOWNLOADS = {
     # filename: md5sum
     'distribute-0.6.49.tar.gz': '89e68df89faf1966bcbd99a0033fbf8e',
     'pip-1.4.1.tar.gz': '6afbb46aeb48abac658d4df742bff714',
-    'virtualenv-1.10.1.tar.gz': '3a04aa2b32c76c83725ed4d9918e362e',
+    'virtualenv-1.11.6.tar.gz': 'f61cdd983d2c4e6aeabb70b1060d6f49',
     'virtualenvwrapper-4.3.tar.gz': 'd526bc0a2cca27d3d37e5c42a4511a1f',
 }
 
