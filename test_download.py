691397ed5bb36ad87407a1aa0cef510ec9770259
jeremy avnet <brainsik-github@theory.org>
2015-04-25 10:28:27 -0700

    Use latest virtualenv

--- test_download.py
+++ test_download.py
@@ -12,7 +12,7 @@ PYPI_DOWNLOADS = {
     # filename: md5sum
     'setuptools-15.1.tar.gz': '10407f6737d8dc37e5310e68c1f1f6ec',
     'pip-1.4.1.tar.gz': '6afbb46aeb48abac658d4df742bff714',
-    'virtualenv-12.0.7.tar.gz': 'e08796f79d112f3bfa6653cc10840114',
+    'virtualenv-12.1.1.tar.gz': '901ecbf302f5de9fdb31d843290b7217',
     'virtualenvwrapper-4.3.1.tar.gz': '4327d04b0e65d4229352454ab8ce3f37',
 }
 
