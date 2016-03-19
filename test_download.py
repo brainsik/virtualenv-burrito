e1932ae31a939d849ebfe57bb508aebacd40aad0
Matt Robenolt <matt@ydekproductions.com>
2016-03-19 16:41:47 -0700

    Remove test dependency on nose

--- test_download.py
+++ test_download.py
@@ -4,7 +4,6 @@ import urllib2
 import csv
 import hashlib
 
-from nose.tools import eq_
 
 PYPI_MD5_URL = 'https://pypi.python.org/pypi?:action=show_md5&digest='
 
@@ -15,6 +14,11 @@ PYPI_DOWNLOADS = {
     'virtualenv-14.0.6.tar.gz': 'a035037925c82990a7659ecf8764bcdb',
     'virtualenvwrapper-4.7.1.tar.gz': '3789e0998818d9a8a4ec01cfe2a339b2',
 }
+
+
+def eq_(a, b, msg=None):
+    if not a == b:
+        raise AssertionError(msg or "%r != %r" % (a, b))
 
 
 def test_tarball_names():
